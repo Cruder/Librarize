046b1a52324e2f3d159ced20cd3a1f59c46f1422
amelie certin <amelie.certin@gmail.com>
2017-06-10 18:47:29 +0200

    Responsify User & encrypt password

--- Librarize.me/models/User.js
+++ Librarize.me/models/User.js
@@ -1,3 +1,6 @@
+'use strict'
+var passwordHash = require('password-hash');
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
     id: {
@@ -26,7 +29,22 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING
     }
   }, {
-    freezeTableName: true
+    freezeTableName: true,
+    instanceMethods: {
+      responsify: function() {
+        let result = {};
+        result.id = this.id;
+        result.lastname = this.lastname;
+        result.firstname = this.firstname;
+        result.email = this.email;
+        return result;
+      }
+    },
+    hooks: {
+      beforeCreate: function(user) {
+        user.password = passwordHash.generate(user.password);
+      }
+    }
   });
   return user;
 };
